02f4e12b7b529d53891fcdcf7c8b35b0f24c1029
Warren Overholt <warren.overholt@uspto.gov>
2016-08-23 12:35:19 -0400

    Have ip6tables fact return nil if matches 1.3

--- lib/facter/ip6tables_version.rb
+++ lib/facter/ip6tables_version.rb
@@ -1,4 +1,24 @@
 Facter.add(:ip6tables_version) do
+  has_weight 100
+  confine :operatingsystem => 'RedHat'
+  confine :lsbmajdistrelease => 5
+  setcode do
+    version = Facter::Util::Resolution.exec('ip6tables --version')
+    if version
+      if (/1\.3\./ =~ version )
+        nil
+      else
+        version.match(/\d+\.\d+\.\d+/).to_s
+      end
+    else
+      nil
+    end
+  end
+end
+
+
+Facter.add(:ip6tables_version) do
+  has_weight 1
   confine :kernel => :linux
   setcode do
     version = Facter::Util::Resolution.exec('ip6tables --version')
