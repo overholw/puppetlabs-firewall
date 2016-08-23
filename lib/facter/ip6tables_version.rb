857f0242db56b5481017ec3772970bdd93b42d30
Warren Overholt <warren.overholt@uspto.gov>
2016-08-23 14:52:48 -0400

    try updating ip6tables provider instead

--- lib/facter/ip6tables_version.rb
+++ lib/facter/ip6tables_version.rb
@@ -1,24 +1,4 @@
 Facter.add(:ip6tables_version) do
-  has_weight 100
-#  confine :operatingsystem => 'RedHat'
-#  confine :lsbmajdistrelease => 5
-  setcode do
-    version = Facter::Util::Resolution.exec('ip6tables --version')
-    if version
-      if (version =~ /1\.3\./)
-        "old"
-      else
-        version.match(/\d+\.\d+\.\d+/).to_s
-      end
-    else
-      nil
-    end
-  end
-end
-
-
-Facter.add(:ip6tables_version) do
-  has_weight 1
   confine :kernel => :linux
   setcode do
     version = Facter::Util::Resolution.exec('ip6tables --version')
